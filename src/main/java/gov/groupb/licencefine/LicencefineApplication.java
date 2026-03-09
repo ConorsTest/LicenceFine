934871d0fd5af782694bdaa0a620cc75902122ac
ConorTest <50129082@belfastmet.ac.uk>
2026-03-09 11:23:19 +0000

    First Commit!

---
+++ src/main/java/gov/groupb/licencefine/LicencefineApplication.java
@@ -0,0 +1,13 @@
+package gov.groupb.licencefine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicencefineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicencefineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/groupb/licencefine/LicencefineApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.groupb.licencefine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencefineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
