8dc56301e402eec12bbd9e91657fd35b06c4b94f
arolston021 <50129162@belfastmet.ac.uk>
2026-03-10 16:29:08 +0000

    Finished Success Page

---
+++ src/main/java/gov/groupb/licencefine/web/SuccessWebController.java
@@ -0,0 +1,15 @@
+package gov.groupb.licencefine.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/success")
+public class SuccessWebController {
+
+    @GetMapping()
+    public String showSuccessPage(){
+        return "success";
+    }
+}
