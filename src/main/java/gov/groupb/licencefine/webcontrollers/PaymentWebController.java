dc4ec95cf0f358c967706c2b3f02901109893db8
ConorTest <50129082@belfastmet.ac.uk>
2026-03-11 14:15:02 +0000

    got input validation on the payment

--- src/main/java/gov/groupb/licencefine/webcontrollers/PaymentWebController.java
+++ src/main/java/gov/groupb/licencefine/webcontrollers/PaymentWebController.java
@@ -2,8 +2,11 @@ package gov.groupb.licencefine.webcontrollers;
 
 import gov.groupb.licencefine.databasecontrollers.Fine;
 import gov.groupb.licencefine.databasecontrollers.FineRepository;
+import gov.groupb.licencefine.validators.CardForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -16,28 +19,38 @@ public class PaymentWebController {
     }
 
     @GetMapping("/{refNum}")
-    public String showPaymentPage(@PathVariable String refNum, Model model) {
+    public String showPaymentPage(@PathVariable String refNum, Model model,
+                                  CardForm cardForm) {
 
         Fine fine = fineRepository.findByRefNum(refNum);
 
-        model.addAttribute("fine", fine);
+        model.addAttribute("fine", fine); //This is still needed for the details on the left hand side of the screen.
+        model.addAttribute("cardForm", cardForm);
 
         return "payment";
     }
 
     @PostMapping("/process")
     public String processPayment(
-            @RequestParam String refNum,
-            @RequestParam String email
-    ) {
+            @Valid CardForm cardForm,
+            BindingResult result,
+            Model model) {
+        if(result.hasErrors()){
+            System.err.println(result.getAllErrors());
+            System.err.println("Oh No");
+            return "payment";
+        }
+        model.addAttribute("Success message",
+                "Details submitted successfully");
 
-        Fine fine = fineRepository.findByRefNum(refNum);
+        Fine fine = fineRepository.findByRefNum(cardForm.getRefNum());
 
-        fine.setEmail(email);
+        fine.setEmail(cardForm.getEmail());
         fine.setPaid(true);
 
         fineRepository.save(fine);
 
+        System.err.println("success");
         return "redirect:/payment/success";
     }
 
