51d299d55d37521415615ab2739107ef7e71d459
arolston021 <50129162@belfastmet.ac.uk>
2026-03-11 10:53:46 +0000

    Added funcitonality to the payment page

--- src/main/java/gov/groupb/licencefine/databasecontrollers/FineRepository.java
+++ src/main/java/gov/groupb/licencefine/databasecontrollers/FineRepository.java
@@ -1,7 +1,8 @@
 package gov.groupb.licencefine.databasecontrollers;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface FineRepository extends CrudRepository<Fine, Long> {
-
+    Fine findByRefNum(String refNum);
 }
--- src/main/java/gov/groupb/licencefine/web/PaymentWebController.java
+++ src/main/java/gov/groupb/licencefine/web/PaymentWebController.java
@@ -1,15 +1,48 @@
 package gov.groupb.licencefine.web;
 
+import gov.groupb.licencefine.databasecontrollers.Fine;
+import gov.groupb.licencefine.databasecontrollers.FineRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/payment")
 public class PaymentWebController {
+    private final FineRepository fineRepository;
 
-    @GetMapping()
-    public String showPaymentPage(){
+    public PaymentWebController(FineRepository fineRepository) {
+        this.fineRepository = fineRepository;
+    }
+
+    @GetMapping("/{refNum}")
+    public String showPaymentPage(@PathVariable String refNum, Model model) {
+
+        Fine fine = fineRepository.findByRefNum(refNum);
+
+        model.addAttribute("fine", fine);
+
         return "payment";
+    }
+
+    @PostMapping("/process")
+    public String processPayment(
+            @RequestParam String refNum,
+            @RequestParam String email
+    ) {
+
+        Fine fine = fineRepository.findByRefNum(refNum);
+
+        fine.setEmail(email);
+        fine.setPaid(true);
+
+        fineRepository.save(fine);
+
+        return "redirect:/payment/success";
+    }
+
+    @GetMapping("/success")
+    public String showSuccessPage() {
+        return "success";
     }
 }
